e670532e7d3377b968a6f1c1598ceff52c390d8d
alufer8673 <alufer8673@ieselcaminas.org>
2024-01-07 23:11:06 +0100

    2024_01_07

---
+++ lua/admin01/init.lua
@@ -0,0 +1,2 @@
+require("admin01.remap")
+require("admin01.set")
---
+++ lua/admin01/set.lua
@@ -0,0 +1,29 @@
+-- numeros relativos
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- 2 tabs
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+-- copiar al portapapeles del os
+vim.opt.clipboard = "unnamedplus"
+
+-- Sin limite de longitud de la linea
+vim.opt.wrap = false
+
+-- cuando busca no se queda con color y mientra busca se pone de color
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- para mejorar los colores
+vim.opt.termguicolors = true
+
+-- el cursor siempre queda con 8 espacios para arriba y para abajo
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
